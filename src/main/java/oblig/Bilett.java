314719d99bc683e1272a5d2b935fd8840ac97ba4
astridholstad <astridholstad21@gmail.com>
2024-04-17 13:43:44 +0200

    jobber

--- src/main/java/oblig/Bilett.java
+++ src/main/java/oblig/Bilett.java
@@ -2,6 +2,18 @@ package oblig;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-public class Bilett {
+
+@RestController
+public class Bilett{
+
 }
+/* {
+    @GetMapping("/")
+    public String hei(String navn){
+        return "hei verden "+ navn;
+    }
+
+}
+*/
